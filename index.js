5bfd76cdabdb95d4d9580917c1868b94d3288d30
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-05-08 20:06:11 +0600

    sent data to the database and got data from the database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,45 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello from node mongo crud server');
 });
+
+// user: dbUser2 pass: fzXxS9H23BX3IOgc
+
+
+const uri = "mongodb+srv://dbUser2:fzXxS9H23BX3IOgc@cluster0.ubvegtf.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const userCollection = client.db('nodeMongoCrud').collection('users');
+
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const user = await cursor.toArray();
+            res.send(user);
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user);
+            console.log(result);
+            res.send(user);
+        });
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
+}
+run().catch(err => console.log(err));
+
 
 app.listen(port, () => {
     console.log(`Mongo server running on port ${port}`);
