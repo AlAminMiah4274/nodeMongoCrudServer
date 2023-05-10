d1b96606f3d797b83f702e99d2228121f998d2c3
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-05-10 18:31:08 +0600

    finished learning crud operations

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware:
 app.use(cors());
 app.use(express.json());
 
@@ -25,11 +26,13 @@ const client = new MongoClient(uri, {
     }
 });
 
+// CRUD operation:
+
 async function run() {
     try {
         const userCollection = client.db('nodeMongoCrud').collection('users');
 
-        // to get data from the database
+        // to get data from the database: Read
         app.get('/users', async (req, res) => {
             const query = {};
             const cursor = userCollection.find(query);
@@ -37,14 +40,39 @@ async function run() {
             res.send(user);
         });
 
-        // to send data to the database
+        // to get single data from the database
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.findOne(query);
+            res.send(result);
+        });
+
+        // to send data to the database: Create
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await userCollection.insertOne(user);
             res.send(user);
         });
 
-        // to delete data from the database
+        // to update data on the database: Update
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const user = req.body;
+            const option = { upsert: true };
+            const updatedUser = {
+                $set: {
+                    name: user.name,
+                    address: user.address,
+                    email: user.email
+                }
+            };
+            const result = await userCollection.updateOne(filter, updatedUser, option);
+            res.send(result);
+        });
+
+        // to delete data from the database: Delete
         app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
